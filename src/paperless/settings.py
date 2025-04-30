701b254b86b5409e8eb35d18122c891da7c2d0cf
shamoon <4887959+shamoon@users.noreply.github.com>
2025-04-30 10:19:02 -0700

    Fix paperless_ai logging

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -887,6 +887,7 @@ LOGGING = {
     "loggers": {
         "paperless": {"handlers": ["file_paperless"], "level": "DEBUG"},
         "paperless_mail": {"handlers": ["file_mail"], "level": "DEBUG"},
+        "paperless_ai": {"handlers": ["file_paperless"], "level": "DEBUG"},
         "ocrmypdf": {"handlers": ["file_paperless"], "level": "INFO"},
         "celery": {"handlers": ["file_celery"], "level": "DEBUG"},
         "kombu": {"handlers": ["file_celery"], "level": "DEBUG"},
